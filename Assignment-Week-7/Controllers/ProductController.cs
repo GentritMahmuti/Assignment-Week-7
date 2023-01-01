b3e577bf7b1b3489e6a8b4df1203ce865645240a
Min HP <Min HP@DESKTOP-NS559A2>
2023-01-01 21:18:45 +0100

    Add project files.

---
+++ Assignment-Week-7/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Assignment_Week_7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+
+namespace Assignment_Week_7.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ElasticClient _elasticClient;
+        public ProductController(ElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+        [HttpPost("elastic")]
+        public async Task<IActionResult> AddElastic()
+        {
+            var product = new Product
+            {
+                ProductName = "Product 1",
+                Description = "Dscription of product 1",
+                Price = 10.99,
+                Category = "Category A"
+            };
+            var result = await _elasticClient.IndexAsync<Product>(product, x => x.Index("products_index").Id(product.ProductName));
+            return Ok(result.Result);
+
+        }
+        [HttpGet("get-elastic-by-id")]
+        public async Task<IActionResult> GetElastic(string product, string index)
+        {
+            var response = await _elasticClient.GetAsync<Product>(product, x => x.Index(index));
+            return Ok(response.Source);
+        }
+
+
+    }
+}
---
+++ Assignment-Week-7/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using Nest;
+
+namespace Assignment_Week_7.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
